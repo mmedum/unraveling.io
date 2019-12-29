c91bc1ee36fe651e00887ba6b8c2073ea90498aa
Mark Medum Bundgaard <mmedum@gmail.com>
2019-12-29 16:16:12 +0100

    Changing to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import ReactGA from 'react-ga';
 import logo from './logo.svg';
 import './App.css';
 
-ReactGA.initialize("UA-155143597-1");
-ReactGA.pageview(window.location.pathname + window.location.search);
-
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
