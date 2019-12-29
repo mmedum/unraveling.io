f141e401ea5ad6eb4f7b9a35d2dfc94616e0e085
Mark Medum Bundgaard <mmedum@gmail.com>
2019-12-29 16:41:37 +0100

    Updating dependencies and google-analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 import logo from './logo.svg';
 import './App.css';
+
+ReactGA.initialize('UA-155143597-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const App: React.FC = () => {
   return (
